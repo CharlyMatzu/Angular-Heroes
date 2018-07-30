60d6218540a1b92fd2785676aceeae3277685941
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2018-07-30 16:57:42 -0700

    Added EventEmitter

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, IHeroe } from '../../services/heroes.service';
-// import { Router } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 
 
@@ -14,15 +15,15 @@ export class HeroesComponent implements OnInit {
   heroes: IHeroe[] = [];
 
   constructor( private _heroesService: HeroesService,
-              // private _router: Router
+              private _router: Router
              ) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
   }
 
-  // verHeroe( index: number ) {
-  //   this._router.navigate( ['/heroe', index] );
-  // }
+  verHeroe( index: number ) {
+    this._router.navigate( ['/heroe', index] );
+  }
 
 }
